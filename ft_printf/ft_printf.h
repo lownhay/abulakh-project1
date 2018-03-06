4a94f6a456613a181582df809a9b85a3b04a4d4f
abulakh <bulah1424@gmail.com>
2018-03-06 15:09:28 +0200

    com1509

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -23,4 +23,28 @@ typedef struct	s_print
 	int hhhllljz[6];
 }				f_print;
 
+typedef struct	s_fordi
+{
+	int mas[7];
+	intmax_t no;
+	signed char hh;
+	short int	h;
+	long int l;
+	long long int ll;
+	intmax_t j;
+	size_t z;
+}				f_fordi;
+
+typedef struct	s_forouxX
+{
+	int mas[7];
+	uintmax_t no;
+	unsigned char hh;
+	unsigned short int h;
+	unsigned long int l;
+	unsigned long long int ll;
+	uintmax_t j;
+	size_t z;
+}				f_forouxX;
+
 #endif
